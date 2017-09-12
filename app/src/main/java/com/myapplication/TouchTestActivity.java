eeda8d5976788598aab6fdf25d362d625a7120ea
rosaemperor <690157675@qq.com>
2017-09-12 16:56:16 +0800

    添加时间选择器

--- app/src/main/java/com/myapplication/TouchTestActivity.java
+++ app/src/main/java/com/myapplication/TouchTestActivity.java
@@ -1,16 +1,21 @@
 package com.myapplication;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.bigkoo.pickerview.TimePickerView;
 import com.myapplication.bean.TestBean;
 import com.myapplication.databinding.ActivityOntouchLayoutBinding;
-import com.myapplication.view.LoadingDialog;
 import com.zhy.autolayout.AutoLayoutActivity;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Administrator on 2017/8/21.
@@ -25,7 +30,7 @@ public class TouchTestActivity extends AutoLayoutActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(TouchTestActivity.this,R.layout.activity_ontouch_layout);
         binding.setEvent(this);
-        LoadingDialog dialog= new LoadingDialog(TouchTestActivity.this);
+//        LoadingDialog dialog= new LoadingDialog(TouchTestActivity.this);
 
     }
     @Override
@@ -38,6 +43,40 @@ public class TouchTestActivity extends AutoLayoutActivity implements View.OnClic
                 TestBean bean= new TestBean();
                 bean.setText("来自点击事件的文字");
                 binding.setTest(bean);
+                Calendar selectedDate = Calendar.getInstance();
+                Calendar startDate = Calendar.getInstance();
+                startDate.set(2013,1,1);
+
+                Calendar endDate = Calendar.getInstance();
+                endDate.set(2100,12,31);
+                TimePickerView timePicker=new TimePickerView.Builder(this, new TimePickerView.OnTimeSelectListener() {
+                    @Override
+                    public void onTimeSelect(Date date, View v) {
+                        Calendar calendar=Calendar.getInstance();
+                        calendar.setTime(date);
+
+                        binding.onTouchTest.setText(calendar.get(Calendar.YEAR)+"");
+                    }
+                }).setType(new boolean[]{true, true, true, true, true, false})
+                        .setCancelText("取消")//取消按钮文字
+                        .setSubmitText("确认")//确认按钮文字
+                        .setContentSize(18)//滚轮文字大小
+                        .setTitleSize(20)//标题文字大小
+                        .setTitleText("")//标题文字
+                        .setOutSideCancelable(true)//点击屏幕，点在控件外部范围时，是否取消显示
+                        .isCyclic(false)//是否循环滚动
+                        .setTitleColor(Color.BLACK)//标题文字颜色
+                        .setSubmitColor(Color.BLACK)//确定按钮文字颜色
+                        .setCancelColor(Color.BLACK)//取消按钮文字颜色
+                        .setTitleBgColor(Color.WHITE)//标题背景颜色 Night mode
+                        .setBgColor(ContextCompat.getColor(TouchTestActivity.this,R.color.c_ffffff))//滚轮背景颜色 Night mode
+                        .setDate(selectedDate)// 如果不设置的话，默认是系统时间*/
+                        .setRangDate(startDate,endDate)//起始终止年月日设定
+                        .setLabel("年","月","日","时","分","秒")//默认设置为年月日时分秒
+                        .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                        .isDialog(false)//是否显示为对话框样式
+                        .build();
+                timePicker.show();
                 break;
             default:
                 break;
