e6abc0dc15426a3b5ed01338a9d04b9574b08cf6
rosaemperor <690157675@qq.com>
2017-09-18 20:04:15 +0800

    自定义interactor

--- app/src/main/java/com/myapplication/view/RemoteControlView.java
+++ app/src/main/java/com/myapplication/view/RemoteControlView.java
@@ -2,9 +2,14 @@ package com.myapplication.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.myapplication.R;
@@ -19,7 +24,16 @@ public class RemoteControlView extends View{
     private int leftColor;
     private int rightColor;
     private int centerColor;
+    private int backgroundColor;
+    private boolean visiableCenter;
+    private String centerText;
+    private int  centerTextSize;
     private int attr;
+    private int widthMesureMode;
+    private int heightMesureMode;
+    private Paint mPaint;
+    private Rect mBound;
+    private RectF backgoundRectF;
     public RemoteControlView(Context context) {
         super(context);
     }
@@ -46,13 +60,65 @@ public class RemoteControlView extends View{
                     topColor = array.getColor(attr,Color.GRAY);
                     break;
                 case R.styleable.RemoteControlViewStyle_leftColor:
-                    leftColor = array.getColor(attr,Color.GRAY);
+                    leftColor = array.getColor(attr,Color.GREEN);
                     break;
-
+                case R.styleable.RemoteControlViewStyle_rightColor:
+                    rightColor = array.getColor(attr,Color.GRAY);
+                    break;
+                case R.styleable.RemoteControlViewStyle_centerColor:
+                    centerColor = array.getColor(attr,Color.RED);
+                    break;
+                case R.styleable.RemoteControlViewStyle_backgroundColor:
+                    backgroundColor = array.getColor(attr,Color.GRAY);
+                    break;
+                case R.styleable.RemoteControlViewStyle_visiCenter:
+                    visiableCenter =array.getBoolean(attr,true);
+                    break;
+                case R.styleable.RemoteControlViewStyle_centerText:
+                    centerText = array.getString(attr);
+                    break;
+                case R.styleable.RemoteControlViewStyle_centerTextSize:
+                    centerTextSize = array.getDimensionPixelSize(attr, (int)TypedValue.applyDimension(TypedValue
+                            .COMPLEX_UNIT_SP,16,getResources().getDisplayMetrics()));
+                    break;
             }
 
         }
+        array.recycle();
+        mPaint = new Paint();
+        mPaint.setTextSize(centerTextSize);
+        mBound =new Rect();
+        mPaint.getTextBounds(centerText,0,centerText.length(),mBound);
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        widthMesureMode = MeasureSpec.getMode(widthMeasureSpec);
+        heightMesureMode = MeasureSpec.getMode(heightMeasureSpec);
+        if(widthMesureMode == MeasureSpec.EXACTLY){
 
+        }else {
+            widthMeasureSpec = mBound.width()*3;
+        }
+        if(heightMesureMode == MeasureSpec.EXACTLY){
+
+        }else {
+            heightMeasureSpec = mBound.height()*3;
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mPaint.setColor(backgroundColor);
+        backgoundRectF = new RectF();
+        backgoundRectF.left=0;
+        backgoundRectF.top=0;
+        backgoundRectF.right=getMeasuredWidth();
+        backgoundRectF.bottom =getMeasuredHeight();
+        canvas.drawRect(backgoundRectF,mPaint);
+        mPaint.setColor(leftColor);
+        canvas.drawArc(backgoundRectF,0,90,true,mPaint);
+
+    }
 }
