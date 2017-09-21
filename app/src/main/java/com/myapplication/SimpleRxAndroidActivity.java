d4eac60c05a058022b7c457624afd656841fa1c9
rosaemperor <690157675@qq.com>
2017-09-21 17:09:45 +0800

    遥控器基本开发完成

--- app/src/main/java/com/myapplication/SimpleRxAndroidActivity.java
+++ app/src/main/java/com/myapplication/SimpleRxAndroidActivity.java
@@ -1,17 +1,11 @@
 package com.myapplication;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.Observable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.FitWindowsLinearLayout;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.myapplication.adapter.TabLayoutPagerAdapter;
@@ -29,6 +23,8 @@ import java.util.List;
 public class SimpleRxAndroidActivity extends AppCompatActivity{
     private RxSimpleTestBinding binding;
     private RxAndroidSimpleViewModel viewModel;
+    float Dx = 0;
+    float Dy = 0;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,4 +43,28 @@ public class SimpleRxAndroidActivity extends AppCompatActivity{
         tab.setCustomView(textView);
     }
 
+//    @Override
+//    public boolean dispatchTouchEvent(MotionEvent ev) {
+//        float x=ev.getX();
+//        float y= ev.getY();
+//
+//        switch (ev.getAction()){
+//            case MotionEvent.ACTION_DOWN:
+//                Dx= ev.getX();
+//                Dy= ev.getY();
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+////                Log.d("AAA","X:"+x+"Y:"+y);
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                double s=Math.abs( Math.sqrt(Math.pow(x-Dx,2)+Math.pow(y-Dy,2)));
+//                Log.d("ACTION_UP","距离："+px2dip(s)/28.346+"厘米");
+//                break;
+//        }
+//        return super.dispatchTouchEvent(ev);
+//    }
+    public int px2dip(Double pxValue) {
+        final float scale =getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
